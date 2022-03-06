7f4cf68c000c096913cb6e5af74104461fdbedf2
Harold Alcala <harold.dev@gmail.com>
2022-03-06 21:39:02 +0800

    initial import

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import { Button, createTheme, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      defaultProps: {
+        variantMapping: {
+          h1: "h2",
+          h2: "h2",
+          h3: "h2",
+          h4: "h2",
+          h5: "h2",
+          h6: "h2",
+          subtitle1: "h2",
+          subtitle2: "h2",
+          body1: "span",
+          body2: "span",
+        },
+      },
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssBaseline />
+      <Types></Types>
+      <TypographyTheme></TypographyTheme>
+      {/* There is already an h1 in the page, let's not duplicate it. */}
+      <Typography variant="h1" component="h2">
+        h1. Heading
+      </Typography>
+      <Typography variant="h3">h1. Heading</Typography>
+      <Typography mt={2}>aaaaa</Typography>
+      <Button variant="contained">Hello World</Button>
     </div>
+  );
+}
+
+const Div = styled("div")(({ theme }) => ({
+  ...theme.typography.button,
+  backgroundColor: theme.palette.background.paper,
+  padding: theme.spacing(1),
+}));
+
+function TypographyTheme() {
+  return <Div>{"This div's text looks like that of a button."}</Div>;
+}
+
+function Types() {
+  return (
+    <Box sx={{ width: "100%", maxWidth: 500 }}>
+      <Typography variant="h1" component="div" gutterBottom>
+        h1. Heading
+      </Typography>
+      <Typography variant="h2" gutterBottom component="div">
+        h2. Heading
+      </Typography>
+      <Typography variant="h3" gutterBottom component="div">
+        h3. Heading
+      </Typography>
+      <Typography variant="h4" gutterBottom component="div">
+        h4. Heading
+      </Typography>
+      <Typography variant="h5" gutterBottom component="div">
+        h5. Heading
+      </Typography>
+      <Typography variant="h6" gutterBottom component="div">
+        h6. Heading
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom component="div">
+        subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+      </Typography>
+      <Typography variant="subtitle2" gutterBottom component="div">
+        subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos
+        laborum fugiat deleniti? Eum quasi quidem quibusdam.
+      </Typography>
+      <Typography variant="body2" gutterBottom>
+        body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos
+        laborum fugiat deleniti? Eum quasi quidem quibusdam.
+      </Typography>
+      <Typography variant="button" display="block" gutterBottom>
+        button text
+      </Typography>
+      <Typography variant="caption" display="block" gutterBottom>
+        caption text
+      </Typography>
+      <Typography variant="overline" display="block" gutterBottom>
+        overline text
+      </Typography>
+    </Box>
   );
 }
 
